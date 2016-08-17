601d7fa5184594405f8fd6e7e78249f4c10beed3
Leonardo Neves <enfimleo@gmail.com>
2016-08-17 07:07:38 -0300

    atirando

--- Assets/scritps/fire.cs
+++ Assets/scritps/fire.cs
@@ -3,13 +3,72 @@ using System.Collections;
 
 public class fire : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public GameObject bullet_prefab;
+    GameObject bullet;
+    float z;
+    public bool player;
+    Vector3 mouseposition;
+
+    Vector3 moveto;
+    Vector3 direction;
+    Vector3 scale;
+    LineRenderer lineRenderer;
+    float startWidth = 1.0f;
+    float endWidth = 1.0f;
+    float threshold = 0.001f;
+    Camera thisCamera;
+    Vector3 lastPos = Vector3.one * float.MaxValue;
+
+    // Use this for initialization
+    void Start () {
+        z = transform.position.z;
+        //gameObject.AddComponent<LineRenderer>();
+        lineRenderer = GetComponent<LineRenderer>();
+        if (player)
+        {
+            lineRenderer.material.color = Color.blue;
+        }
+        else
+        {
+            lineRenderer.material.color = new Color(0.9f, 0.9f, 0.9f, 0.5f);
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, z);
+        mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+
+        if (player == variables.turno)
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.SetWidth(startWidth, endWidth);
+            lineRenderer.SetVertexCount(2);
+            direction = new Vector2(mouseposition.x - transform.position.x, mouseposition.y - transform.position.y);
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, direction);
+
+            if(Input.GetMouseButtonDown(1)){
+                GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                bullet = Instantiate<GameObject>(bullet_prefab);
+                bullet.transform.position = transform.position;
+                scale = mouseposition - transform.position;
+                float speed = scale.magnitude * 0.3f; //maxspeed
+                bullet.GetComponent<Rigidbody2D>().AddForce(direction * speed, ForceMode2D.Impulse);
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+
+        
+    }
 }
--- Assets/scritps/move.cs
+++ Assets/scritps/move.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class move : MonoBehaviour {
     float z;
     int speed = 15;
     public bool player;
     Vector3 mouseposition;
-    float minimaldistance = 11;
+    float minimaldistance = 20;
+    Vector3 moveto;
+    Vector3 screen;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         z = transform.position.z;
-	}
+        screen = new Vector3(Screen.width, Screen.height, z);
+        screen = Camera.main.ScreenToWorldPoint(screen);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,27 +25,32 @@ public class move : MonoBehaviour {
         mouseposition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, z);
         mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
 
+        if (Math.Abs(transform.position.x) >= screen.x)
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        if (Math.Abs(transform.position.y) >= screen.y)
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+
         if ( player==variables.turno  &&  Vector3.Distance(mouseposition,transform.position) < minimaldistance )
         {
             if (Input.GetMouseButton(0) && Vector2.Distance(mouseposition, transform.position) > 0)
             {
                 transform.position = mouseposition;
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                GetComponent<Rigidbody2D>().angularVelocity = 0;
 
-            }
-
-            if (Input.GetMouseButton(1))
-            {
             }
         }
 
 
         if (player == variables.turno)        {
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-            transform.position += move * speed * Time.deltaTime;
+            moveto = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+            transform.position += moveto * speed * Time.deltaTime;
         }
 
 
 
-        }
+
+     }
 }
