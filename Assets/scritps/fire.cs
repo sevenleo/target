d5e73a675d8b498f9a64cf9b783141187415b556
Leonardo Neves <enfimleo@gmail.com>
2016-08-23 12:25:24 -0300

    water-material

--- Assets/scritps/fire.cs
+++ Assets/scritps/fire.cs
@@ -63,22 +63,22 @@ public class fire : MonoBehaviour {
             if (Input.GetMouseButtonDown(0)){
                 GetComponent<Rigidbody2D>().velocity = Vector2.zero;
                 GetComponent<Rigidbody2D>().angularVelocity = 0;
-
             }
 
             if (Input.GetMouseButton(0) && Vector2.Distance(mouseposition, transform.position) >= variables.minimaldistance)
             {
                 bullet = Instantiate<GameObject>(bullet_prefab);
+                bullet.tag = player ? "p1" : "p2";
                 Physics2D.IgnoreCollision(bullet.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
                 bullet.transform.position = transform.position;
                 scale = mouseposition - transform.position;
                 speed = scale.magnitude * 0.2f; //maxspeed
                 bullet.GetComponent<Rigidbody2D>().AddForce(direction * speed, ForceMode2D.Impulse);
-                Color randomcolor = new Color(0, Random.Range(0, 0.3f), Random.Range(.3f, 1f), Random.Range(.3f, 1f));
+                Color randomcolor = new Color(0, Random.Range(0, 0.4f), Random.Range(.6f, 1f), Random.Range(.3f, 1f));
                 bullet.GetComponent<SpriteRenderer>().color = randomcolor;
                 gameObject.GetComponentInChildren<SpriteRenderer>().color = randomcolor;
                 lineRenderer.SetColors(randomcolor, Color.black);
-                bullet.transform.SetParent (gameObject.transform);
+                //bullet.transform.SetParent (gameObject.transform);
                 Destroy(bullet, 8);
             }else
             {
@@ -99,10 +99,10 @@ public class fire : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-
-        if (coll.gameObject.transform.parent != gameObject.transform)
+        if (coll.gameObject.tag != gameObject.tag)
         {
-            coll.gameObject.transform.parent.transform.localScale = coll.gameObject.transform.parent.transform.localScale + one;
+            //coll.gameObject.transform.parent.transform.localScale = coll.gameObject.transform.parent.transform.localScale + one;
+            
             //Camera.main.backgroundColor = Color.red;
             //collisionTime = Time.time;
         }
